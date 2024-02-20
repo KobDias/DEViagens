d5433777261ba44ac0f72064daddd683e8b81ca8
Kobdias <sofhia.dias@etec.sp.gov.br>
2024-02-20 19:25:21 -0300

    Primeiro commit

---
+++ Index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/trabalhomeudeus/css/style.css">
+    <title>Agencia de viagens</title>
+    <link rel="icon" type="image/x-icon" href="/trabalhomeudeus/Assets/59118.png">
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="/trabalhomeudeus/">Planos de viagem</a></li>
+                <li><a href="">Sobre nós</a></li>
+                <li><a href="">Fale conosco</a></li>
+            </ul>
+        </nav>
+        <div id="nome">
+            <p> DEViagens</p>
+        </div>
+    </header>
+    <main>
+        <div class="lado-esq">
+
+        </div>
+        <div class="lado-dir">
+            
+        </div>
+    </main>
+</body>
+</html>
